7a324d7d49ff7a3bd15d8ce2b86d2a36903c9707
KhanhIceTea <khanhicetea@gmail.com>
2018-08-21 18:06:45 +0700

    Init version

---
+++ README.md
@@ -0,0 +1,44 @@
+# SMTPor
+
+Never need a bigass mail client to test your SMTP config !
+
+![SMTPor](https://user-images.githubusercontent.com/4528223/31022947-991302c2-a564-11e7-9ad6-13fddde7c46e.png)
+
+## TL;DR
+
+Using Docker to run
+
+```bash
+$ docker run -d -p 3000:3000 khanhicetea/smtpor
+```
+
+Then open **http://localhost:3000** to try a shot !
+
+## Install & Start
+
+```bash
+$ npm install
+$ npm start
+```
+
+## Heroku Deploying
+
+```bash
+$ heroku login
+$ heroku git:remote -a [your-app-name]
+$ git push heroku master
+```
+
+## Contributors
+
+- @khanhicetea
+
+## License
+
+MIT License
+
+-----------------------------------
+
+**Ref:**
+
+- Cover photo from [MageeWP](https://www.mageewp.com/)
---
+++ main.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require("crypto");
+const util = require('util');
+const nodemailer = require('nodemailer');
+const moment = require('moment');
+
+const checkSignature = function(value, sign, secret) {
+  const hmac = crypto.createHmac("sha256", secret);
+  return sign == hmac.update(value.toString()).digest("hex");
+};
+
+const rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+
+const SECRET_KEY = process.env.SECRET_KEY || '';
+const SMTP_HOST = process.env.SMTP_HOST || 'smtp.gmail.com';
+const SMTP_PORT = parseInt(process.env.SMTP_PORT || 587);
+const SMTP_ENCRYPT = process.env.SMTP_ENCRYPT || 'tls';
+const SMTP_USER = process.env.SMTP_USER || '';
+const SMTP_PASSWORD = process.env.SMTP_PASSWORD || '';
+const SMTP_FROM = process.env.SMTP_FROM || '';
+const SMTP_TO = process.env.SMTP_TO || '';
+
+const app = express();
+
+app.use(bodyParser.json({ verify: rawBodySaver }));
+app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
+app.use(bodyParser.raw({ verify: rawBodySaver, type: function () { return true } }));
+
+app.post('/send/:app/:signature', function(req, res) {
+  const app = req.params.app;
+  const signature = req.params.signature;
+
+  if (!checkSignature(app, signature, SECRET_KEY)) {
+    res.status(400);
+    return res.end();
+  }
+
+  const secured = {'tls': 2, 'ssl': 1}[SMTP_ENCRYPT] || 0;
+
+  const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    secure: (secured == 1 ? true : false),
+    requireTLS: (secured == 2 ? true : false),
+    auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD
+    }
+  });
+
+  const now = moment().format('DD-MM-YY HH:mm');
+  const body = req.body.text || req.rawBody;
+  const mailOptions = {
+    from: SMTP_FROM,
+    to: SMTP_TO,
+    subject: util.format('[%s] New message from #%s', now, app),
+    text: body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500);
+      return res.end();
+    } else {
+      return res.send('OK!');
+    }
+  });
+});
+
+app.listen(3000, '127.0.0.1');
